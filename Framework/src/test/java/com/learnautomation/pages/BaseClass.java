2e32c3d2c3663d39ed09b86eb4aa90de283db199
inc2011 <inc2011@TEMS-CTXHSD1.TEXILAORG.COM>
2021-05-11 18:06:09 +0530

    changes made on 11/5/2021

--- Framework/src/test/java/com/learnautomation/pages/BaseClass.java
+++ Framework/src/test/java/com/learnautomation/pages/BaseClass.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -42,11 +43,13 @@ public class BaseClass {
 		
 		Reporter.log("Setting Done and Test can be Started",true);
 	}
+	@Parameters({"browser","url"})
 	@BeforeClass
-	public void setup() {
+	public void setup(String browser,String url) {
 		Reporter.log("Trying to start browser and Getting application ready ",true);
-		driver=Browserfactory.startApplication(driver,config.getBrowser(),config.getStaggingURL());	
+		//driver=Browserfactory.startApplication(driver,config.getBrowser(),config.getStaggingURL());
 		
+		driver=Browserfactory.startApplication(driver,browser,url);
 		Reporter.log("Browser and  Application is up and running ",true);
 	}
 	@AfterClass
